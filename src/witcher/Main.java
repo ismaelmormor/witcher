1bba54984afaf71706bf15b8c22d2e31ca7101df
Ismael <ismaelmormor@gmail.com>
2023-01-18 12:35:13 +0100

    Nueva Clase main

---
+++ src/witcher/Main.java
@@ -0,0 +1,18 @@
+package witcher;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Witcher brujo= new Witcher();
+		brujo.setName("Willy wonka");
+		brujo.setHealth(1);
+		brujo.setHasSoul(true);
+		if(brujo.isDeath()) {
+			System.out.println(brujo.getName()+" está muerto");
+		}else {
+			System.out.println(brujo.getName()+" está vivo");
+		}
+	}
+
+}
--- src/witcher/Witcher.java
+++ src/witcher/Witcher.java
@@ -35,17 +35,5 @@ public class Witcher {
 		}
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Witcher brujo= new Witcher();
-		brujo.setName("Willy wonka");
-		brujo.setHealth(1);
-		brujo.setHasSoul(true);
-		if(brujo.isDeath()) {
-			System.out.println(brujo.getName()+" está muerto");
-		}else {
-			System.out.println(brujo.getName()+" está vivo");
-		}
-	}
 
 }
